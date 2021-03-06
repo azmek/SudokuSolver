dd91623c8129428e99212d75214f564d75f2279e
Aziz Meknassi <azizmek@gmail.com>
2021-03-06 13:27:58 +0000

    added a gui version

--- sudokuSolver.py
+++ sudokuSolver.py
@@ -1,3 +1,5 @@
+import pprint
+
 board = [
     [7,8,0,4,0,0,1,2,0],
     [6,0,0,0,7,5,0,0,9],
@@ -10,34 +12,39 @@ board = [
     [0,4,9,2,0,6,0,0,7]
 ]
 
-def solve(bo):
-    find = find_empty(bo)
+def solve(brd):
+    """
+    Solves a sudoku board using backtracking
+    :param brd: 2d list of ints
+    :return: solution
+    """
+    find = find_empty(brd)
     if not find:
         return True
     else:
         row, col = find
 
     for i in range(1,10):
-        if valid(bo, i, (row, col)):
-            bo[row][col] = i
+        if valid(brd, i, (row, col)):
+            brd[row][col] = i
 
-            if solve(bo):
+            if solve(brd):
                 return True
 
-            bo[row][col] = 0
+            brd[row][col] = 0
 
     return False
 
 
-def valid(bo, num, pos):
+def valid(brd, num, pos):
     # Check row
-    for i in range(len(bo[0])):
-        if bo[pos[0]][i] == num and pos[1] != i:
+    for i in range(len(brd[0])):
+        if brd[pos[0]][i] == num and pos[1] != i:
             return False
 
     # Check column
-    for i in range(len(bo)):
-        if bo[i][pos[1]] == num and pos[0] != i:
+    for i in range(len(brd)):
+        if brd[i][pos[1]] == num and pos[0] != i:
             return False
 
     # Check box
@@ -46,7 +53,7 @@ def valid(bo, num, pos):
 
     for i in range(box_y*3, box_y*3 + 3):
         for j in range(box_x * 3, box_x*3 + 3):
-            if bo[i][j] == num and (i,j) != pos:
+            if brd[i][j] == num and (i,j) != pos:
                 return False
 
     return True
@@ -70,7 +77,3 @@ def find_empty(brd):
             if brd[i][j] == 0:
                 return (i, j)               
     return None
-print_board(board)
-solve(board)
-print("___________________")
-print_board(board)
